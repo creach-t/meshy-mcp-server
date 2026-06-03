4e0b5e81bf90bc817a9a965373163f7d411ba938
CREACH-T <142243266+creach-t@users.noreply.github.com>
2026-06-03 16:23:21 +0200

    build: add compiled dist with image_urls fix

---
+++ dist/constants.d.ts
@@ -0,0 +1,9 @@
+import { TaskStatus, TaskType, ResponseFormat } from "./types.js";
+export { TaskStatus, TaskType, ResponseFormat };
+export declare const POLL_INITIAL_DELAY: number;
+export declare const POLL_MAX_DELAY: number;
+export declare const POLL_BACKOFF_FACTOR: number;
+export declare const POLL_FINALIZATION_DELAY: number;
+export declare const CHARACTER_LIMIT: number;
+export declare const RETRY_ATTEMPTS: number;
+export declare const RETRY_DELAY: number;
